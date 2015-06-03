ff145b442b095a067572d47842a0705f9e696512
harveenbhatia <vinny_1612@yahoo.co.in>
2015-06-02 19:25:50 -0700

    Harveen  Bhatia changes
    made changes to wireup app.stache and app.js as requested in the problem
    challenge

--- app.js
+++ app.js
@@ -4,9 +4,27 @@ steal(
     function(can) {
 
         // YOUR CODE HERE
-
+       var contactListViewModel =  {
+		   inputData :{name: "", email: "", phone: ""},
+		   tableData : new can.List(),
+		   onAdd: function(){
+			   this.tableData.push(
+			   {
+				   name: this.inputData.name,
+				   email: this.inputData.email,
+				   phone: this.inputData.phone
+			   }
+			   );
+			
+			this.inputData.attr("name","");
+			this.inputData.attr("email","");
+			this.inputData.attr("phone","");
+		   }		
+		   
+	   };
        return can.Component.extend({
-           tag: 'intro-app'
+           tag: 'intro-app',
+		   viewModel: contactListViewModel
        });
         
         
